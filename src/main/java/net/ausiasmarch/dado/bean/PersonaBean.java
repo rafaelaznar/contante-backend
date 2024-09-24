e4969f5e9a63d407f98d0d47a9a00396a9a4c17b
Rafael Aznar <rafaaznar@gmail.com>
2024-09-24 18:11:15 +0200

    add personas classes

---
+++ src/main/java/net/ausiasmarch/dado/bean/PersonaBean.java
@@ -0,0 +1,41 @@
+package net.ausiasmarch.dado.bean;
+
+public class PersonaBean {
+    String nombre;
+    String apellido1;
+    String apellido2;
+    String email;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
