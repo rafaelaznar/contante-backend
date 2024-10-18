5ed3804f555f2724f7a5b346dfdef6dd70b2f95d
Rafael Aznar <rafaaznar@gmail.com>
2024-10-18 18:43:29 +0200

    filtro

--- src/main/java/net/ausiasmarch/dado/api/Usuario.java
+++ src/main/java/net/ausiasmarch/dado/api/Usuario.java
@@ -1,5 +1,7 @@
 package net.ausiasmarch.dado.api;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.ausiasmarch.dado.entity.UsuarioEntity;
@@ -27,8 +30,10 @@ public class Usuario {
     UsuarioService oUsuarioService;
 
     @GetMapping("")
-    public ResponseEntity<Page<UsuarioEntity>> getPage(Pageable oPageable) {
-        return new ResponseEntity<Page<UsuarioEntity>>(oUsuarioService.getPage(oPageable), HttpStatus.OK);
+    public ResponseEntity<Page<UsuarioEntity>> getPage(
+            Pageable oPageable,
+            @RequestParam  Optional<String> filter) {
+        return new ResponseEntity<Page<UsuarioEntity>>(oUsuarioService.getPage(oPageable, filter), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
--- src/main/java/net/ausiasmarch/dado/service/UsuarioService.java
+++ src/main/java/net/ausiasmarch/dado/service/UsuarioService.java
@@ -1,5 +1,7 @@
 package net.ausiasmarch.dado.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -30,14 +32,22 @@ public class UsuarioService {
             oUsuarioEntity.setNombre(arrNombres[getRandomInt(0, arrNombres.length - 1)]);
             oUsuarioEntity.setApellido1(arrApellidos[getRandomInt(0, arrApellidos.length - 1)]);
             oUsuarioEntity.setApellido2(arrApellidos[getRandomInt(0, arrApellidos.length - 1)]);
-            oUsuarioEntity.setEmail("email" + oUsuarioEntity.getNombre() + getRandomInt(999,9999) + "@gmail.com");
+            oUsuarioEntity.setEmail("email" + oUsuarioEntity.getNombre() + getRandomInt(999, 9999) + "@gmail.com");
             oUsuarioRepository.save(oUsuarioEntity);
         }
         return oUsuarioRepository.count();
     }
 
-    public Page<UsuarioEntity> getPage(Pageable oPageable) {
-        return oUsuarioRepository.findAll(oPageable);
+    public Page<UsuarioEntity> getPage(Pageable oPageable, Optional<String> filter) {
+
+        if (filter.isPresent()) {
+            return oUsuarioRepository
+                    .findByNombreContainingOrApellido1ContainingOrApellido2ContainingOrEmailContaining(
+                            filter.get(), filter.get(), filter.get(), filter.get(),
+                            oPageable);
+        } else {
+            return oUsuarioRepository.findAll(oPageable);
+        }
     }
 
     public UsuarioEntity get(Long id) {
