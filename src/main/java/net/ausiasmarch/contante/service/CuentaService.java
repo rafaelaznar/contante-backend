f6d8b41146638c36d99a86d273c6838aa01cbf75
HIKWR <ikerffcontacto@gmail.com>
2024-11-11 18:33:35 +0100

    creado Cuenta

---
+++ src/main/java/net/ausiasmarch/contante/service/CuentaService.java
@@ -0,0 +1,88 @@
+package net.ausiasmarch.contante.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import net.ausiasmarch.contante.entity.CuentaEntity;
+import net.ausiasmarch.contante.exception.ResourceNotFoundException;
+import net.ausiasmarch.contante.repository.CuentaRepository;
+
+@Service
+public class CuentaService implements ServiceInterface<CuentaEntity> {
+
+    @Autowired
+    CuentaRepository oCuentaRepository;
+
+    @Autowired
+    RandomService oRandomService;
+
+    private String[] arrCodigo = {"1234", "5678", "9012", "3456", "7890"};
+    
+    
+
+
+    public Long randomCreate(Long cantidad) {
+        for (int i = 0; i < cantidad; i++) {
+            CuentaEntity oCuentaEntity = new CuentaEntity();
+            oCuentaEntity.setCodigo(arrCodigo[oRandomService.getRandomInt(0, arrCodigo.length - 1)]);
+            oCuentaEntity.setDescripcion("");
+            oCuentaRepository.save(oCuentaEntity);
+        }
+        return oCuentaRepository.count();
+    }
+
+    public Page<CuentaEntity> getPage(Pageable oPageable, Optional<String> filter) {
+
+        if (filter.isPresent()) {
+            return oCuentaRepository
+                    .findBycodigoContainingOrid_tipocuentaContaining(
+                            filter.get(), filter.get(),
+                            oPageable);
+        } else {
+            return oCuentaRepository.findAll(oPageable);
+        }
+    }
+
+    public CuentaEntity get(Long id) {
+        return oCuentaRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Cuenta no encontrado"));
+        // return oCuentaRepository.findById(id).get();
+    }
+
+    public Long count() {
+        return oCuentaRepository.count();
+    }
+
+    public Long delete(Long id) {
+        oCuentaRepository.deleteById(id);
+        return 1L;
+    }
+
+    public CuentaEntity create(CuentaEntity oCuentaEntity) {
+        return oCuentaRepository.save(oCuentaEntity);
+    }
+
+    public CuentaEntity update(CuentaEntity oCuentaEntity) {
+        CuentaEntity oCuentaEntityFromDatabase = oCuentaRepository.findById(oCuentaEntity.getId()).get();
+        if (oCuentaEntity.getCodigo() != null) {
+            oCuentaEntityFromDatabase.setCodigo(oCuentaEntity.getCodigo());
+        }
+        if (oCuentaEntity.getDescripcion() != null) {
+            oCuentaEntityFromDatabase.setDescripcion(oCuentaEntity.getDescripcion());
+        }
+        if (oCuentaEntity.getIdTipoCuenta() != null) {
+            oCuentaEntityFromDatabase.setIdTipoCuenta(oCuentaEntity.getIdTipoCuenta());
+        }
+        return oCuentaRepository.save(oCuentaEntityFromDatabase);
+    }
+
+    public Long deleteAll() {
+        oCuentaRepository.deleteAll();
+        return this.count();
+    }
+
+}
