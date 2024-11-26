c3b6cdcf3a40ebfad59f28219c3f61514053db9e
MrCobi <148904685+MrCobi@users.noreply.github.com>
2024-11-26 20:53:41 +0100

    AsientoxPeriodo (#28)

--- src/main/java/net/ausiasmarch/contante/repository/AsientoRepository.java
+++ src/main/java/net/ausiasmarch/contante/repository/AsientoRepository.java
@@ -17,6 +17,12 @@ public interface AsientoRepository extends JpaRepository<AsientoEntity, Long> {
       String strComentarios, Long id_usuario, Pageable oPageable);
 
   Page<AsientoEntity> findByUsuarioId(Long id_usuario, Pageable oPageable);
+  
+  @Query(value = "SELECT * FROM periodo WHERE (descripcion LIKE %:strDescripcion% OR comentarios LIKE %:strComentarios%) AND id_periodo=:id_periodo", nativeQuery = true)
+  Page<AsientoEntity> findByPeriodoIdAndDescripcionContainingOrComentariosContaining(String strDescripcion,
+      String strComentarios, Long id_periodo, Pageable oPageable);
+
+  Page<AsientoEntity> findByPeriodoId(Long id_periodo, Pageable oPageable);
 
   @Query(value = "SELECT * FROM asiento WHERE (descripcion LIKE %:strDescripcion% OR comentarios LIKE %:strComentarios%) AND id_tipoasiento=:id_tipoasiento", nativeQuery = true)
   Page<AsientoEntity> findByTipoasientoIdAndDescripcionContainingOrComentariosContaining(String strDescripcion,
