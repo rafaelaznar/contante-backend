8520e81fd677816bd1902a12a7123d232e6c9591
Rafael Aznar <rafaaznar@gmail.com>
2024-11-26 19:58:26 +0100

    cuenta xtipocuenta

--- src/main/java/net/ausiasmarch/contante/repository/CuentaRepository.java
+++ src/main/java/net/ausiasmarch/contante/repository/CuentaRepository.java
@@ -1,15 +1,21 @@
 package net.ausiasmarch.contante.repository;
 
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import net.ausiasmarch.contante.entity.CuentaEntity;
 
 public interface CuentaRepository extends JpaRepository<CuentaEntity, Long> {
 
-    Page<CuentaEntity> findByCodigoContainingOrDescripcionContaining (
-           String filter1, String filter2, Pageable oPageable);
-}
+    Page<CuentaEntity> findByCodigoContainingOrDescripcionContaining(
+            String filter1, String filter2, Pageable oPageable);
 
+    @Query(value = "SELECT * FROM cuenta WHERE (codigo LIKE %:strCodigo% OR descripcion LIKE %:strDescripcion%) AND id_tipocuenta=:id_tipocuenta", nativeQuery = true)
+    Page<CuentaEntity> findByTipocuentaIdAndCodigoContainingOrDescripcionContaining(
+            Long id_tipocuenta, String strCodigo, String strDescripcion, Pageable oPageable);
+
+    Page<CuentaEntity> findByTipocuentaId(Long id_tipocuenta, Pageable oPageable);
+    
+}
