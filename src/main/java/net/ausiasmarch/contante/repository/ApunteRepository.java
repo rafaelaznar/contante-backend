294e722727123e2ed1af1d9edfca104bf342c1c8
EloyML05 <148987947+EloyML05@users.noreply.github.com>
2024-12-02 17:18:24 +0100

    parte de las sumas a medias (#33)
    * relacion tipoApunte to apunte
    
    * parte de las sumas a medias
    
    ---------
    
    Co-authored-by: EloyML05 <eloymitalopez3@gmai.com>

--- src/main/java/net/ausiasmarch/contante/repository/ApunteRepository.java
+++ src/main/java/net/ausiasmarch/contante/repository/ApunteRepository.java
@@ -4,30 +4,40 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import net.ausiasmarch.contante.entity.ApunteEntity;
+import net.ausiasmarch.contante.entity.SumasProjection;
 
 public interface ApunteRepository extends JpaRepository<ApunteEntity, Long> {
 
-    Page<ApunteEntity> findByDescripcionContainingOrComentariosContaining(
-            String filter1, String filter2, Pageable oPageable);
+        Page<ApunteEntity> findByDescripcionContainingOrComentariosContaining(
+                        String filter1, String filter2, Pageable oPageable);
 
-    @Query(value = "SELECT * FROM apunte WHERE (descripcion LIKE %:strDescripcion% OR comentarios LIKE %:strComentarios%) AND id_asiento=:id_asiento", nativeQuery = true)
-    Page<ApunteEntity> findByAsientoIdAndDescripcionContainingOrComentariosContaining(String strDescripcion,
-            String strComentarios, Long id_asiento, Pageable oPageable);
+        @Query(value = "SELECT * FROM apunte WHERE (descripcion LIKE %:strDescripcion% OR comentarios LIKE %:strComentarios%) AND id_asiento=:id_asiento", nativeQuery = true)
+        Page<ApunteEntity> findByAsientoIdAndDescripcionContainingOrComentariosContaining(String strDescripcion,
+                        String strComentarios, Long id_asiento, Pageable oPageable);
 
-    Page<ApunteEntity> findByAsientoId(Long id_asiento, Pageable oPageable);
+        Page<ApunteEntity> findByAsientoId(Long id_asiento, Pageable oPageable);
 
-    @Query(value = "SELECT * FROM apunte WHERE (descripcion LIKE %:strDescripcion% OR comentarios LIKE %:strComentarios%) AND id_tipoapunte=:id_tipoapunte", nativeQuery = true)
-    Page<ApunteEntity> findByTipoApunteIdAndDescripcionContainingOrComentariosContaining(String strDescripcion,
-            String strComentarios, Long id_tipoapunte, Pageable oPageable);
+        @Query(value = "SELECT * FROM apunte WHERE (descripcion LIKE %:strDescripcion% OR comentarios LIKE %:strComentarios%) AND id_tipoapunte=:id_tipoapunte", nativeQuery = true)
+        Page<ApunteEntity> findByTipoApunteIdAndDescripcionContainingOrComentariosContaining(String strDescripcion,
+                        String strComentarios, Long id_tipoapunte, Pageable oPageable);
 
-    Page<ApunteEntity> findByTipoapunteId(Long id_tipoapunte, Pageable oPageable);
+        Page<ApunteEntity> findByTipoapunteId(Long id_tipoapunte, Pageable oPageable);
 
-    @Query(value = "SELECT * FROM apunte WHERE descripcion LIKE %:strDescripcion% OR comentarios LIKE %:strComentarios% AND id_subcuenta=:id_subcuenta", nativeQuery = true)
-    Page<ApunteEntity> findBySubcuentaIdAndDescripcionContainingOrComentariosContaining(String strDescripcion,
-        String strComentarios, Long id_subcuenta, Pageable oPageable);
+        @Query(value = "SELECT * FROM apunte WHERE descripcion LIKE %:strDescripcion% OR comentarios LIKE %:strComentarios% AND id_subcuenta=:id_subcuenta", nativeQuery = true)
+        Page<ApunteEntity> findBySubcuentaIdAndDescripcionContainingOrComentariosContaining(String strDescripcion,
+                        String strComentarios, Long id_subcuenta, Pageable oPageable);
 
-    Page<ApunteEntity> findBySubcuentaId(Long id_subcuenta, Pageable oPageable);
+        Page<ApunteEntity> findBySubcuentaId(Long id_subcuenta, Pageable oPageable);
 
+        @Query(value = "SELECT sum(debe) AS totalDebe, sum(haber) AS totalHaber FROM apunte WHERE id_asiento = :id_asiento", nativeQuery = true)
+   SumasProjection totalByAsientoId(@Param("id_asiento") Long id_asiento);
+
+        @Query(value = "SELECT sum(debe) AS totalDebe, sum(haber) AS totalHaber  FROM apunte WHERE id_subcuenta=:id_subcuenta", nativeQuery = true)
+        SumasProjection totalBySubcuentaId(Long id_subcuenta);
+
+        @Query(value = "SELECT sum(debe) AS totalDebe, sum(haber) AS totalHaber  FROM apunte WHERE id_tipoapunte = :id_tipoapunte", nativeQuery = true)
+        SumasProjection totalByTipoapunteId(Long id_tipoapunte);
 }
